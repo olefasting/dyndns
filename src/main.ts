af7dd2e1eec7e799196bdb98dfbd6e9136318e06
Ole A. Sjo Fasting <oasf@kaliyuga.io>
2025-03-15 05:46:37 +0100

    change digitalocean client provider

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
-import "dotenv/config";
-import fetch from "node-fetch";
-import { createClient } from "digitalocean-api-client";
+import "@std/dotenv/load";
+
+import { createApiClient } from "dots-wrapper";
 
 if (process.env.DYNDNS_DISABLE && process.env.DYNDNS_DISABLE === "1") {
 	console.log("dyndns disabled (DYNDNS_DISABLE is set to '1')");
 	process.exit(0);
 }
 
-const domains = process.env.DYNDNS_DOMAINS;
-if (!domains) {
+const records: string[] = (process.env.DYNDNS_RECORDS || "")
+	.split(",")
+	.filter((v) => v !== "");
+if (records.length === 0) {
 	console.error(
-		"domains could not be found in env (use DYNDNS_DOMAINS or a .env file)",
+		"records could not be found in env (use DYNDNS_RECORDS or a .env file)",
 	);
 	process.exit(1);
 }
@@ -23,85 +25,86 @@ if (!dotoken) {
 	process.exit(1);
 }
 
-const client = createClient({
-	token: dotoken,
-});
+const dots = createApiClient({ token: dotoken });
 
-for (const rec of domains.split(",")) {
-	let domain: string | undefined = undefined;
-	let subdomain: string | undefined = undefined;
-	let segments = rec.split(".");
-	if (segments.lenth > 2) domain = segments.join(".");
-	const records_res = await client.GET("/v2/domains/{domain_name}/records", {
-		params: {
-			path: {
-				domain_name: "kaliyuga.io", //domain,
-			},
-			query: {
-				name: "home.kaliyuga.io", //subdomain,
-				type: "A",
-			},
-		},
-	});
+const echo_ip_url = process.env.DYNDNS_ECHO_IP_URL || "https://api.ipify.org";
 
-	if (records_res.error && records_res.response.status !== 404) {
-		console.error("error when retrieving existing record", records_res.error);
-		process.exit(1);
-	} else if (
-		records_res.response.status !== 404 &&
-		(!records_res.data ||
-			!records_res.data.domain_records ||
-			records_res.data.domain_records.length <= 0)
-	) {
-		console.error(
-			"server returned an invalid response or an empty list of domain records",
-			records_res,
-		);
-		process.exit(1);
+const ip_res = await fetch(echo_ip_url);
+
+const current_ip = (await ip_res.text()).trim();
+
+try {
+	const {
+		data: { domains },
+	} = await dots.domain.listDomains({});
+	for (const record of records) {
+		const domain = domains.find((v) => record.endsWith(v.name));
+		if (domain !== undefined) {
+			const domain_name = domain.name;
+			try {
+				const {
+					data: { domain_records },
+				} = await dots.domain.listDomainRecords({
+					domain_name,
+					name: record,
+					type: "A",
+				});
+				const name = record.substring(
+					0,
+					record.length - domain_name.length - 1,
+				);
+				const existing_record = domain_records.find((v) => v.name === name);
+				if (existing_record === undefined) {
+					try {
+						const {
+							data: { domain_record },
+						} = await dots.domain.createDomainRecord({
+							data: current_ip,
+							domain_name,
+							name,
+							type: "A",
+						});
+						console.log(`created new record for '${record}'`, domain_record);
+					} catch (error) {
+						console.error(`error when creating record for '${record}'`, error);
+						process.exit(1);
+					}
+				} else if (existing_record.data !== current_ip) {
+					try {
+						const {
+							data: { domain_record },
+						} = await dots.domain.updateDomainRecord({
+							data: current_ip,
+							domain_name,
+							domain_record_id: existing_record.id,
+						});
+						console.log(
+							`updated existing record for '${record}'`,
+							domain_record,
+						);
+					} catch (error) {
+						console.error(
+							`error when updating existing record for '${record}'`,
+							error,
+						);
+						process.exit(1);
+					}
+				} else {
+					console.log(`record for '${record}' already points to current ip`);
+				}
+			} catch (error) {
+				console.error(
+					`error when listing domain records for domain '${domain_name}'`,
+					error,
+				);
+				process.exit(1);
+			}
+		}
 	}
-
-	const echo_ip_url = process.env.DYNDNS_ECHO_IP_URL || "https://api.ipify.org";
-	console.log("before", records_res);
-	const record = records_res.data.domain_records[0];
-	const ip_res = await fetch(echo_ip_url);
-
-	const record_id = record.id;
-
-	const current_ip = (await ip_res.text()).trim();
-	const record_ip = record.data.trim();
-
-	if (current_ip === record_ip) {
-		console.log(`DNS record is already pointing to your IP (${current_ip})`);
-		process.exit(0);
-	}
-
-	const update_res = await client.PUT(
-		"/v2/domains/{domain_name}/records/{record_id}",
-		{
-			headers: {
-				Accept: "application/json",
-				"Content-Type": "application/json",
-			},
-			params: {
-				path: {
-					domain_name: domain,
-					record_id: record_id,
-				},
-			},
-			body: {
-				type: "A",
-				data: current_ip.trim(),
-			},
-		},
-	);
-
-	if (update_res.error) {
-		console.error("error when updating existing record", update_res.error);
-		process.exit(1);
-	} else if (!update_res.data) {
-		console.error("server returned an invalid response", update_res.data);
-		process.exit(1);
-	}
-
-	const updated_record = update_res.data.domain_record;
+} catch (error) {
+	console.error("error when listing domains", error);
+	process.exit(1);
 }
+
+console.log("success!");
+process.exit(0);
